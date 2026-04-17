0fb96844ef5a4971f8dfe6bae0273cc25ff82a09
danielsartor1599 <danielsartor1599@gmail.com>
2026-04-16 22:13:43 -0300

    lol

---
+++ 01JavaScript/testes.js
@@ -0,0 +1,23 @@
+
+// alert("Olá Mundo!!")
+// alert("SEGUNDA MG S")
+
+// let nome
+
+// nome = "Daniel"
+
+// alert("Nome: " + nome)
+
+// let idade = 30
+// alert("idade: " + idade)
+
+// alert("Nome: " + nome + "\nidade: " + idade)
+
+// nome = prompt("Digite seu nome:")
+// alert("Nome digitado: " + nome)
+
+let nota1
+nota1 = prompt("Qual é a nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
