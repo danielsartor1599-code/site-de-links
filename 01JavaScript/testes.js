7ecf2e9924138fd465801bd0d4e98e105e761280
danielsartor1599 <danielsartor1599@gmail.com>
2026-04-23 20:32:52 -0300

    [Peba ]e o campeonato de futebol

--- 01JavaScript/testes.js
+++ 01JavaScript/testes.js
@@ -16,8 +16,25 @@
 // nome = prompt("Digite seu nome:")
 // alert("Nome digitado: " + nome)
 
-let nota1
-nota1 = prompt("Qual é a nota: ")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+// let nota1
+// nota1 = prompt("Qual é a nota: ")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// // alert(nota1)
+
+// =============================================
+
+let vitorias, empates
+let pontos
+
+// leituras
+vitorias = prompt("vitórias: ")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Empates: "))
+
+// processamentos
+pontos = vitorias*3 + empates*1
+
+// saidas
+alert("Total de pontos: " + pontos)
