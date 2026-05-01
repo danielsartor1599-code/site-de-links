044b0001546c26d9f80f1bd75e658d1a32b55668
danielsartor1599 <danielsartor1599@gmail.com>
2026-04-30 21:18:27 -0300

    dona bete

---
+++ 01JavaScript/Sarumano.js
@@ -0,0 +1,15 @@
+// informações
+let shows,bombas
+quantidade
+
+// leituras
+
+shows = Number(prompt("Quantidades de Shows: "))
+
+// processamentos
+
+quantidade = shows + bombas*7
+
+// saídas
+
+alert("Bombas necessarias: " + quantidade)
