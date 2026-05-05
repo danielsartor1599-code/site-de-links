e13cd91c3faf6bc482086bc77774c23f0f2d888d
danielsartor1599 <danielsartor1599@gmail.com>
2026-05-04 22:11:28 -0300

    Continuar

--- 02.CSS/romeroBrito.js
+++ 02.CSS/romeroBrito.js
@@ -1,3 +1,52 @@
+
+let resposta = document.getElementById('resultado')
+function DiaDaSemana(){
+    // informações
+    let numero = Math.ceil
+
+    let chute = Number(prompt("Qual o Número: "))
+
+    if(chute == numero){resposta.innerHTML = "segunda"}
+}
+function verificandoIdade(){
+    
+    // informações
+    let idade
+
+    // entradas
+
+    idade = Number(prompt("Digite sua Idade: "))
+
+    // processamentos
+    // saídas
+
+    if(idade >= 18){
+        resposta.innerHTML = "Você é maior de Idade"}
+        
+        else{resposta.innerHTML = "Você é menor de Idade:"}
+    }
+
+function verificarProvisoes(){
+    
+    // infos
+    let marujos,comida
+    let comidaPorMarujo
+
+    // entradas
+
+    marujos = Number(prompt("Quantidade de Marujos: "))
+    comida = Number(prompt("Quilos de comidas: "))
+
+    // processamentos
+
+    comidaPorMarujo = comida / marujos
+
+    // saídas
+    if(marujos >= 10 && comidaPorMarujo >= 1.5){ // ||
+        document.getElementById('resultado').innerHTML = "<br>Provisões suficientes. <br>Rumo ao horizonte!"}
+    
+    else{document.getElementById('resultado').innerHTML = "<br>Algo está errado. <br>Ninguém no Mar hoje."}
+}
 function calcularPrecoBrique(){
     
     // informações
